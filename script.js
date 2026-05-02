aac1dd148c47d991a29d14ead8706dcac5e5042d
CindyKim0519 <bokyeong2018@gmail.com>
2026-05-02 15:12:05 +0900

    Refine support confirm modal

--- script.js
+++ script.js
@@ -2674,9 +2674,9 @@ function openSupportSubmitNotice() {
   modal.insertAdjacentHTML("beforeend", `
     <div class="support-submit-overlay" role="dialog" aria-modal="true">
       <div class="support-submit-sheet">
-        <h3>문의를 보내시겠어요?</h3>
+        <h3>문의 내용을 보낼까요?</h3>
         <p>문의 답변은 알림으로 앱 안에서 확인할 수 있어요.</p>
-        <div class="row">
+        <div class="support-submit-actions">
           <button class="ghost-btn" type="button" data-support-submit-cancel>취소</button>
           <button class="primary-btn" type="button" data-support-submit-confirm>보내기</button>
         </div>
