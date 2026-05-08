96b1b6ddcd13c26d75e3f0151f9c879ce9b9b86f
CindyKim0519 <bokyeong2018@gmail.com>
2026-05-09 01:35:12 +0900

    Clarify anniversary alert scope

--- script.js
+++ script.js
@@ -2630,7 +2630,7 @@ function openAnniversarySettingsPage() {
         <section class="card">
           <h3>자동 기념일</h3>
           <p>우리의 시작일을 기준으로 100일 단위와 주년을 자동으로 계산해요.</p>
-          <p>D-7, D-1, D-day에 홈과 알림에서 알려주고, 홈에는 대표 기념일 1개만 표시합니다.</p>
+          <p>자동 기념일과 추가한 기념일은 D-7, D-1, D-day에 홈과 알림에서 알려주고, 홈에는 대표 기념일 1개만 표시합니다.</p>
         </section>
         <section class="card">
           <h3>추가한 기념일</h3>
